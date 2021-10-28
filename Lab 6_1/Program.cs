175cda050a5a7a2a282f89c8ff8accbd2bdd2ce9
ПОльзователь <@USER436809363>
2021-10-28 13:18:12 +0300

    Добавьте файлы проекта.

---
+++ Lab 6_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_6_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            string max = " ";
+            foreach (string l in stringArray)
+            {
+                if (l.Length > stringArray.Length)
+                    max = l;
+            }
+            Console.WriteLine("Самое длинное слово: {0}",max);
+            Console.ReadKey();
+        }
+    }
+}
